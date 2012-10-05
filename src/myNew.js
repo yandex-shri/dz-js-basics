2a0bd1d6583aeaba8703f465330d00385bf86a66
Shuhrat <shadmonkulov@gmail.com>
2012-10-05 23:43:20 +0400

    Updated according to the recommendations of @azproduction

--- src/myNew.js
+++ src/myNew.js
@@ -9,20 +9,14 @@ function myNew() {
   if (args.length) {
     fun = args.shift();
     if (typeof fun !== 'function') {
-      throw {
-        'name': 'TypeError',
-        'message': 'Constructor function is not found'
-      }
+      throw new TypeError('Constructor function is not found');
     }
     obj = {
       '__proto__': fun.prototype
     }
     fun.apply(obj, args);
   } else {
-    throw {
-      'name': 'TypeError',
-      'message': 'Must be at least one argument'
-    }
+    throw new TypeError('Must be at least one argument');
   }
 
   return obj;
